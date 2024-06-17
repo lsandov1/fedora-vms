331ecc574b43c12a9efe3a4d049a400ca09a69fb
Leo Sandoval <lsandova@redhat.com>
2024-06-17 12:10:47 -0600

    switch to SB
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- rawhide-aarch64.mk
+++ rawhide-aarch64.mk
@@ -3,6 +3,6 @@ include Makefile
 virtname=rawhide-aarch64
 location=http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/aarch64/os/
 os_variant=fedora-rawhide
-secure_boot=no
+secure_boot=yes
 serial=file
 ks_file=install.ks
