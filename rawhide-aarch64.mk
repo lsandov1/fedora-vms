1e6c5ec2053db5ffa12fb4adf7ce2687626a0aae
Leo Sandoval <lsandova@redhat.com>
2024-06-17 11:59:50 -0600

    switch serial to file for rawhide-aarch64
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- rawhide-aarch64.mk
+++ rawhide-aarch64.mk
@@ -4,5 +4,5 @@ virtname=rawhide-aarch64
 location=http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/aarch64/os/
 os_variant=fedora-rawhide
 secure_boot=no
-serial=pty
+serial=file
 ks_file=install.ks
