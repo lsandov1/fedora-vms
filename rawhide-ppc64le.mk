c4195b898fe41665749910e6f9b8d0927dec4bf9
Leo Sandoval <lsandova@redhat.com>
2024-06-18 16:05:06 -0600

    disable secure boot for ppc64le
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- rawhide-ppc64le.mk
+++ rawhide-ppc64le.mk
@@ -3,6 +3,6 @@ include Makefile
 virtname=rawhide-ppc64le
 location=http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/ppc64le/os/
 os_variant=fedora-rawhide
-secure_boot=yes
+secure_boot=no
 serial=file
 ks_file=install.ks
