0095fc866d8b5712b004cad599865e2e595632dd
Leo Sandoval <lsandova@redhat.com>
2024-06-18 15:59:18 -0600

    track rawhide-ppc64le
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

---
+++ rawhide-ppc64le.mk
@@ -0,0 +1,8 @@
+include Makefile
+
+virtname=rawhide-ppc64le
+location=http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/ppc64le/os/
+os_variant=fedora-rawhide
+secure_boot=yes
+serial=file
+ks_file=install.ks
