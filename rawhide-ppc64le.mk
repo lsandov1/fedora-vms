15f1eafb1710ecd18d2b94a57842f351829d74e5
Leo Sandoval <lsandova@redhat.com>
2024-06-18 16:30:38 -0600

    set a serial pty
    for some reason ssh service is not up
    
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- rawhide-ppc64le.mk
+++ rawhide-ppc64le.mk
@@ -3,5 +3,5 @@ include Makefile
 virtname=rawhide-ppc64le
 location=http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/ppc64le/os/
 os_variant=fedora-rawhide
-serial=file
+serial=pty
 ks_file=install.ks
