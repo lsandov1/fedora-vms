c329863e302796a5b6405ef5f3215da9069b72af
Leo Sandoval <lsandova@redhat.com>
2024-07-05 13:44:18 -0600

    track new files
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

---
+++ rhel9.4.0-lvm.mk
@@ -0,0 +1,9 @@
+include Makefile
+
+virtname=rhel-9.4.0-lvm-final
+location=http://download.eng.bos.redhat.com/released/RHEL-9/9.4.0/BaseOS/x86_64/os/
+os_variant=rhel9.4
+boot=no_sb
+serial=pty
+ks_file=install_lvm.ks
+device_name=vdy
