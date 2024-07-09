eb747b3e4672e8ba2c8ff133d9e5149a41794e62
Leo Sandoval <lsandova@redhat.com>
2024-07-09 13:08:01 -0600

    aarch64 arch machines does not support no_sb boot
    + sudo virt-install --os-variant fedora-rawhide --name fedora-rawhide-aarch64-RHEL-15032-rebase-grub2-on-rc2.12.mk --memory 4096 --vcpus 2 --disk size=10 --boot loader=/usr/share/edk2/ovmf/OVMF_CODE.secboot.fd,loader_ro=yes,loader_type=pflash,nvram_template=/usr/share/edk2/ovmf/OVMF_VARS.fd,loader_secure=no,bootmenu.enable=on,bios.useserial=on --features smm.state=on --machine q35 --noautoconsole --graphics none --serial pty --initrd-inject=/root/fedora-vms/install.ks --extra-args 'console=ttyS0 inst.ks=file:/install.ks' --location=http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/aarch64/os/
    ERROR    Host does not support domain type None with machine 'q35' for virtualization type 'hvm' with architecture 'aarch64'
    
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- fedora-rawhide-aarch64-RHEL-15032-rebase-grub2-on-rc2.12.mk
+++ fedora-rawhide-aarch64-RHEL-15032-rebase-grub2-on-rc2.12.mk
@@ -1,5 +1,6 @@
 virtname := $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
 location := http://download.eng.bos.redhat.com/released/fedora/rawhide/latest-Fedora-Rawhide/Everything/aarch64/os/
 os_variant :=fedora-rawhide
+boot := sb
 
 include Makefile
